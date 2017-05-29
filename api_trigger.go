08a78bfca7835938e7eaf2cace918bcf94579f51
Ronny Bangsund <github@neuralshortcircuit.com>
2017-05-29 12:01:53 +0200

    Triggers accept credentials and the name of a trigger (repository), then pull down the latest data. Nothing fancy, no checkouts of branches.

---
+++ api_trigger.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+)
+
+// apiTrigger pulls the latest update for a repository and notifies any watchers.
+// TODO: Actually implement watchers.
+func apiTrigger(w http.ResponseWriter, r *http.Request) error {
+	q := r.URL.Query()
+	username := q.Get("user")
+	token := q.Get("token")
+	username = strings.Trim(username, "\n ")
+	user := users.Get(username)
+	if user == nil {
+		return errors.New("unknown user " + username)
+	}
+
+	if user.Token != token {
+		return errors.New("token mismatch for user " + username)
+	}
+
+	name := q.Get("name")
+	name = strings.Trim(name, "\n ")
+	trigger := triggermap[name]
+	info("Updating %s (%s) from %s", trigger.Name, trigger.Path, trigger.URL)
+	err := gitPull(trigger.Path)
+	return err
+}
